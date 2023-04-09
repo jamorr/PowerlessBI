11d46ef132c3c0226c40a8414e65c5fc5c14a563
jamorr <55964176+jamorr@users.noreply.github.com>
2023-04-09 14:41:41 -0400

    rm legacy functions and added watchdog support

--- PowerlessBI/main.py
+++ PowerlessBI/main.py
@@ -1,10 +1,12 @@
-import json
 from os import chdir
+import os
 from os.path import abspath, dirname
+from tkinter import messagebox
 from typing import Any, Tuple
 
 import pandas as pd
 from customtkinter import CTk, StringVar
+from padding import Padding
 
 from data_manager import DataManager  # noqa: F401
 from data_selection_page import SelectData
@@ -63,7 +65,7 @@ class WindowManager(CTk):
         self.grid_rowconfigure(0, weight=1)
 
         self.data_manager = DataManager()
-        self.path_settings: dict
+        # self.path_settings: dict
         self.selected_data = StringVar()
 
         self.selection_window_exists = False
@@ -80,33 +82,42 @@ class WindowManager(CTk):
         Place selection window frames in grid\n
         """
         self.grid_columnconfigure(1, weight=0)
-        self.wm_geometry("")
-        self.load_json()
+        # self.load_json()
 
         if not self.selection_window_exists:
             # configure comands and init selection_window
             self.selection_window = SelectData(
-                self, self.path_settings, self.data_types, self.type_requirements)
+                self,
+                # self.path_settings,
+                # self.data_types,
+                self.data_manager,
+                self.type_requirements
+            )
             self.selection_window.read_frame.import_button.configure(
                 command=self.import_window_grid)
             self.selection_window.read_frame.data_opt.configure(
                 variable=self.selected_data)
+            # configure button commands for vis creation to
+            # call vis with same name as button text
             for text, button in self.selection_window.ver_frame.vis_buttons.items():
-                button.configure(
-                    command=(lambda text=text: self.gen_var_window(text)))
-
+                button.configure(command=(lambda text=text: self.gen_var_window(text)))
+            self.data_manager.bind(
+                "data_selection",
+                self.update_save_list_in_data_selection_page
+            )
+            self.update_save_list_in_data_selection_page()
+            # only create sel_window once
             self.selection_window_exists = True
         else:
             self.selection_window.grid()
 
-        # if selected key is not in the current list of keys then reset selected
-        if self.selected_data.get() not in list(self.path_settings.keys()):
+    def update_save_list_in_data_selection_page(self):
+        if self.selected_data.get() not in (saves := self.data_manager.get_saves()):
             self.selected_data.set(value='')
-            self.selection_window.read_frame.data_opt.configure(
-                values=list(self.path_settings.keys()))
+        else:
+            self.selection_window.read_dtypes(self.selected_data.get())
 
-        # fix this
-        # self.selection_window.updateButtons()
+        self.selection_window.read_frame.data_opt.configure(values=saves)
 
     def import_window_grid(self):
         """Create import window, remove selection window widgets,
@@ -121,6 +132,14 @@ class WindowManager(CTk):
             self.import_window = ImportWindow(self, self.data_manager)
             self.import_window.file_frame.home_button.configure(
                 command=self.import_window_forget_grid)
+            self.import_window.grid(
+                row=0, column=0,
+                sticky='nsew',
+                padx=(0, 16)
+            )
+            # resizes window
+            self.wm_geometry("")
+
         else:
             self.import_window.grid()
             self.import_window.view_frame.grid()
@@ -133,43 +152,26 @@ class WindowManager(CTk):
         self.import_window.view_frame.grid_remove()
         self.sel_window_grid()
 
-    # load json into self.path_settings as dict
-    def load_json(self):
-        """load json into self.path_settings as dict
-        """
-
-        try:
-            with open('../path_settings.json', 'r') as f:
-                self.path_settings = json.loads(f.read())
-        except FileNotFoundError:
-            default = {}
-            with open('../path_settings.json', 'x') as f:
-                f.write(json.dumps(default))
-            self.path_settings = default
-
-        try:
-            with open('../type_dict.json', 'r') as f:
-                self.data_types = json.loads(f.read())
-        except FileNotFoundError:
-            # File was not found, so create the file and write some default data to it
-            default_data = {}
-            with open('../type_dict.json', 'x') as f:
-                f.write(json.dumps(default_data))
-            self.data_types = default_data
-
     def gen_var_window(self, vis_type):
         """generate window to select options for graph,
         including X and target variables, colors, and seeds.
         """
         self.selection_window.grid_remove()
         self.var_window = VarWindow(
-            self, vis_type,
-            self.data_types[self.selected_data.get()],
+            self,
+            vis_type,
+            self.data_manager.load_selected_dtypes(self.selected_data.get()),
             self.type_requirements[vis_type]
         )
         self.var_window.home_button.configure(command=self.del_var_window)
         self.var_window.right_frame.gen_button.configure(
             command=self.create_vis)
+        self.var_window.grid(
+            row=0, column=0,
+            padx=Padding.LARGE,
+            pady=Padding.LARGE,
+            sticky="nsew"
+        )
 
     def del_var_window(self):
         self.var_window.destroy()
@@ -186,10 +188,28 @@ class WindowManager(CTk):
         vis_type = "_".join(self.var_window.vis_type.lower().split(' ')) + "_plot"
         # instead of data_frame here we could use data manager
         # to load in the data potentially
-        data_frame = pd.read_csv(
-            **self.path_settings[self.selected_data.get()])
+        settings = self.data_manager.load_selected_settings(self.selected_data.get())
+
+        # match file type to dataframe
+        # TODO: #4 add addtional support for other parameters
+        parameter_names = [
+            "filepath_or_buffer",
+            "io",
+            "path",
+            "query",
+            "path_or_buf"
+        ]
+        for par_name in parameter_names:
+            if par_name in settings:
+                file_type = os.path.basename(settings[par_name]).split('.')[1]
+                break
+        else:
+            messagebox.showerror("ERROR", "Invalid or missing filepath or buffer.")
+            return
+
+        read_method = getattr(pd, "read_"+file_type)
+        data_frame = read_method(**settings)
         PlotData(x_vars, y_var, color, vis_type, data_frame, master=self)
-        pass
 
 
 if __name__ == '__main__':
