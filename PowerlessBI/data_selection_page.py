e386b07588645860217156ec19b1befe97f7af62
jamorr <55964176+jamorr@users.noreply.github.com>
2023-04-09 14:42:47 -0400

    added data_manager support and cleaned imports

--- PowerlessBI/data_selection_page.py
+++ PowerlessBI/data_selection_page.py
@@ -1,5 +1,7 @@
-from padding import Padding
 from customtkinter import CTk, CTkButton, CTkFrame, CTkOptionMenu
+from data_manager import DataManager
+from padding import Padding
+
 # from collections import Counter
 """
 https://numpy.org/doc/stable/_images/dtype-hierarchy.png
@@ -34,17 +36,23 @@ class ReadFrame(CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=0)  # type: ignore
         # add button to open import window
-        self.import_button = CTkButton(self, text='Import New',
-                                       fg_color='#517f47',
-                                       hover_color='#385831')
-        self.import_button.grid(column=0, row=0,
-                                padx=Padding.LARGE,
-                                pady=Padding.TOP)
+        self.import_button = CTkButton(
+            self, text='Import New',
+            fg_color='#517f47',
+            hover_color='#385831'
+        )
+        self.import_button.grid(
+            column=0, row=0,
+            padx=Padding.LARGE,
+            pady=Padding.TOP
+        )
         # dropdown selection to pick data set
         self.data_opt = CTkOptionMenu(self)
-        self.data_opt.grid(column=0, row=1,
-                           padx=Padding.LARGE,
-                           pady=Padding.BOTTOM)
+        self.data_opt.grid(
+            column=0, row=1,
+            padx=Padding.LARGE,
+            pady=Padding.BOTTOM
+        )
 
 
 # create a frame to display what regression/classification/visualization
@@ -52,7 +60,11 @@ class ReadFrame(CTkFrame):
 
 class VerFrame(CTkFrame):
 
-    def __init__(self, master: CTkFrame, type_requirements: list[str]):
+    def __init__(
+        self,
+        master: CTkFrame,
+        type_requirements: list[str]
+    ):
         super().__init__(master)
 
 
@@ -62,7 +74,7 @@ class VerFrame(CTkFrame):
 
         for vis_type in type_requirements:
             button = CTkButton(self, text=vis_type,
-                               fg_color=["gray90", "gray13"],
+                               fg_color=("gray90", "gray13"),
                                state='disabled',)
             button.grid(column=0, padx=Padding.LARGE,
                         pady=Padding.SMALL, sticky='ns')
@@ -85,8 +97,9 @@ class SelectData(CTkFrame):
     def __init__(
         self,
         master: CTk,
-        path_settings:dict,
-        data_types:dict = {},
+        # path_settings:dict,
+        # data_types:dict = {},
+        data_manager:DataManager,
         type_requirements:dict[str, dict] = {}
     ) -> None:
 
@@ -95,9 +108,10 @@ class SelectData(CTkFrame):
         self.grid_rowconfigure((0, 1), weight=0)  # type: ignore
         self.grid(sticky='nsew')
 
-        self.data_types = data_types
-        self.path_settings = path_settings
-        self.dtypes_count = None
+        # self.data_types = data_types
+        # self.path_settings = path_settings
+        self.data_manager = data_manager
+        self.dtypes_count:dict
 
         # num = numerical, ord = ordinal, cat = categorical, time = datetime,
         # any = any of the above
@@ -126,7 +140,7 @@ class SelectData(CTkFrame):
         # get counts for
         temp_types_dict = self.dtypes_count.copy()
 
-        for k, v in required.items():
+        for v in required.values():
             num, *types = v
             if 'any' in types:
                 # Check if there are enough variables of any type available
@@ -157,10 +171,12 @@ class SelectData(CTkFrame):
 
 
     def read_dtypes(self, selected):
-        self.dtypes_count = {key:len(value)
-                             for key, value in self.data_types[selected].items()}
-
+        # self.dtypes_count = {key:len(value)
+        #                      for key, value in self.data_types[selected].items()}
+        self.dtypes_count = {
+            key:len(value)
+            for key, value in self.data_manager.load_selected_dtypes(selected).items()
+        }
         for text, button in self.ver_frame.vis_buttons.items():
             required = self.type_requirements[text]
             self.update_buttons(required, button)
-
